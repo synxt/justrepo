4b0a5a0a720a73588f46836f3fb3daa486eb158b
synxt <krishna@synxt.in>
2022-04-08 21:38:58 +0530

    Feature One Accepted

--- helloworld/src/main/java/in/synxt/helloworld/App.java
+++ helloworld/src/main/java/in/synxt/helloworld/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World From Main!" );
         System.out.println( "Hello World Feature One!" );
     }
 }
