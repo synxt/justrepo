a3bec18b5ddc827ccc091c5431aebb2764b610a3
Synxt <krishna@synxt.in>
2022-04-08 21:21:11 +0530

    Feature One Commit

--- helloworld/src/main/java/in/synxt/helloworld/App.java
+++ helloworld/src/main/java/in/synxt/helloworld/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World From Main!" );
         System.out.println( "Hello World Feature One!" );
+        System.out.println("Hello World Feature Two!");
     }
 }
